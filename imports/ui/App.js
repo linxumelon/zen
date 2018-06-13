3eb412d940f2b778bd0bc3ef42288c4a10ab9654
LIN XU <mokka.lin@outlook.com>
2018-06-13 15:55:25 +0800

    Tried to create App.js and main.js

---
+++ imports/ui/App.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+      return (
+        <div className="container">
+          <h1>
+            <header>
+              ZEN COLORING
+            </header>
+          </h1>
+    
+          {/* <div style = "float: left ;width: 40%;">
+            <ul>
+              <p>
+                <img src="images/sample.jpg"/>
+              </p>
+            </ul>
+          </div>
+    
+          <div style = "float: right; width: 55%;">
+            <ul>
+              <p class="subTheme">
+                <br></br>
+                <img src="images/IMG_0133.jpg"/>
+                  Multi-Area Selection
+              </p>
+
+              <p class="descTheme">
+                Select multiple areas to color simultaneously.
+              </p> 
+
+              <p class="subTheme">
+                <img src="images/IMG_lineSelection.jpg"/>
+                  Line Coloring
+              </p>
+              <p class="descTheme">
+                The lines can be colored as well.
+              </p>
+          
+              <p class="subTheme">
+                <img src="images/IMG_upload.jpg"  />
+                  Upload
+              </p>
+              <p class="descTheme">
+                Upload your own templates; Use templates contributed by other users.
+              </p>
+          
+              <p class="subTheme">
+                <img src="images/IMG_save.jpg"/>
+                  Save
+              </p>
+              <p class="descTheme">
+                Save your art, and do whatever you want with it.
+              </p>
+            </ul>
+          </div>
+    
+          <br></br>
+          <div>
+            <div class="buttons">
+              <a href="chooseTemplate.html">
+              <button>
+                <p class="buttonTop">START</p>
+                <p class="buttonBottom">Choose an existing template</p>
+              </button>
+              </a>
+            </div>
+    
+            <div class="buttons">
+              <a href="upload.html">
+              <button>
+                <p class="buttonTop">UPLOAD</p>
+                <p class="buttonBottom">Upload your own template</p>
+              </button>
+              </a>
+            </div> 
+          </div> */}
+      </div>
+      )
+  }
+
+}
