77e876e3cbac05f8817a3fd19eb3a8efe99c44dd
LIN XU <mokka.lin@outlook.com>
2018-06-17 16:44:14 +0900

    Added render of colorPage to start button; still not working

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,6 +1,71 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+  constructor() {
+    super();
+  }
+
+  handleClickOnStartButton() {
+    return (
+      <div>
+        <h1>
+          <div class = "theme">
+            ZEN COLORING
+          </div>
+        </h1>
+        
+        <div class="coloringBody">
+          <div class="vert-bar">
+            <canvas id="picker"></canvas>
+              <br></br>
+              <input id="color" value="54aedb"></input>
+
+                const KellyColorPicker = {
+                  place: 'picker'，
+                  input: 'color',
+                  size : 150
+                
+            </div>
+  
+            <div class="pickButton">
+              <button>Brush</button>
+              <button>Fill</button>
+            </div>
+               
+            <div class="sizeSlider">
+              <p> Brush size </p>
+                <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+            </div>
+
+            <div class="opacitySlider">
+              <p> Brush opacity </p>
+              <input type="range" min="1" max="100" value="50" class="slider" id="myRange"></input>
+            </div>
+         
+            <p> Color Lineart </p>
+            <div class="pickButton">
+              <button>On</button>
+              <button>Off</button>
+            </div>
+          </div>
+  
+          <div class="horiz-bar">
+            <button><img src="https://d30y9cdsu7xlg0.cloudfront.net/png/177491-200.png" style="width:50px;height:50px;-webkit-filter: invert(100%);"/></button>
+            <button><img src="https://d30y9cdsu7xlg0.cloudfront.net/png/189086-200.png" style="width:52px;height:50px;-webkit-filter: invert(100%);"/></button>
+            <button><img src="https://d30y9cdsu7xlg0.cloudfront.net/png/189086-200.png" style="width:52px;height:50px;-webkit-transform: scaleX(-1);
+            transform: scaleX(-1);-webkit-filter: invert(100%);"/></button>
+            <button><img src="http://simpleicon.com/wp-content/uploads/zoom-in.png" style="width:30px;height:30px;-webkit-filter: invert(100%); margin: 10px;"/>	 </button>
+            <button><img src="http://simpleicon.com/wp-content/uploads/zoom-out.png" style="width:30px;height:30px;-webkit-filter: invert(100%); margin: 10px;"/></button>
+            <div class="pickButton" style="float:left;">
+              <button> Select </button>
+              <button> De-select </button>
+            </div>
+            <button style="height:53px;padding-left:10px;padding-right:10px;"> Save </button>
+            </div>
+    </div>
+    )
+   
+  }
   render() {
       return (
         <div>
@@ -55,7 +120,7 @@ export default class App extends Component {
     
           <div className ="buttons">
               <a href="chooseTemplate.html">
-              <button>
+              <button onClick={this.handleClickOnStartButton.bind(this)}>
                 <p className="buttonTop">START</p>
                 <p className="buttonBottom">Choose an existing template</p>
               </button>
@@ -71,5 +136,6 @@ export default class App extends Component {
       </div>
       )
   }
-
 }
+
+ 
