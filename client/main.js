329c7ec25c98840628d523091cab7e8532525d7a
LIN XU <mokka.lin@outlook.com>
2018-06-18 22:17:28 +0900

    Added Images=new FS.Collection

--- client/main.js
+++ client/main.js
@@ -4,6 +4,10 @@ import { render } from 'react-dom';
 
 import App from '../imports/ui/App.js';
 
+Images = new FS.Collection("images", {
+  stores: [new FS.Store.FileSystem("images", {path: "~/upload"})]
+});
+
 Meteor.startup(() => {
   render(<App />, document.getElementById('render-target'))
 });
--- server/main.js
+++ server/main.js
@@ -2,6 +2,9 @@ import { Meteor } from 'meteor/meteor';
 
 import '../imports/api/colorTemplates.js';
 
+Images = new FS.Collection("images", {
+  stores: [new FS.Store.FileSystem("images", {path: "~/uploads"})]
+});
 Meteor.startup(() => {
   // code to run on server at startup
 });
