d86827750dbf048633cdbc0c4cc7f7913396f47c
Xiao Yilin <>
2018-06-19 21:24:31 +0800

    made upload page work

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -6,9 +6,6 @@ import { Template } from 'meteor/templating';
 
 import ReactDOM from 'react-dom';
 
-import Images from '../../server/main.js';
-
-import UploadForm from '../upload/uploadPage.js';
 
 export default class App extends Component {
   constructor(props) {
@@ -154,6 +151,57 @@ export default class App extends Component {
         </h1>
         {page}
       </div>  
+    );  
+  }
+}
+
+class UploadForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isPublic: false,
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(event) {
+    this.setState({
+      isPublic: !this.state.isPublic
+    });
+  }
+
+  handleSubmit(event) {
+    let image = event.target.files[0];
+    if (this.state.isPublic) {
+      fsFile = new FS.File(image);
+      Images.insert(fsFile, function(err, fileObj) {
+        if (!err) {
+          const imageID = fileObj._id;
+        }
+      });
+    } else {
+
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="uploadContainer">
+          <p> Upload a .png file with no background </p>
+          <form onSubmit={this.handleSubmit}>
+            <input type="file" name="uploadedImage" className="uploadFileInput"
+              ref={input => {
+                this.fileInput = input;
+              }}
+            />
+            <p><input type="checkbox" name="shareSettings" checked={this.state.isPublic} onChange={this.handleInputChange}/> Make this template public </p>      
+            <input type="submit" name="startColoring" value="Start Coloring"/>
+          </form>
+        </div>
+      </div>
     );  
   }
 }
