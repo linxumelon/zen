83202740abf092be9258547d7aa719a6beb88236
Xiao Yilin <>
2018-06-24 22:57:34 +0800

    display images in choose templates page

--- commons/collection.js
+++ commons/collection.js
@@ -1,9 +1,15 @@
+
+var createThumb = function(fileObj, readStream, writeStream) {
+  //  transforms image into 300x300px thumbnail
+  gm(readStream,fileObj.name()).resize('300','300' + '^').gravity('Center').extent('300', '300').stream().pipe(writeStream);
+};
+
 var imageStore = new FS.Store.GridFS("images");
-// var thumbStore = new FS.Store.GridFS("thumbs", {path:" /uploads/thumbs", transformWrite: createThumb });
+var thumbStore = new FS.Store.GridFS("thumbs", {path:" /uploads/thumbs", transformWrite: createThumb });
 Images = new FS.Collection("images", {
   stores: [
 		imageStore,
-		// thumbStore
+		thumbStore
   ],
   filter: {
   	allow: {
