6f61c2d8620d19413954c6eac52e47ad796d8f93
Xiao Yilin <>
2018-06-24 23:06:34 +0800

    fixed some styling

--- commons/collection.js
+++ commons/collection.js
@@ -4,7 +4,12 @@ var createThumb = function(fileObj, readStream, writeStream) {
   gm(readStream,fileObj.name()).resize('300','300' + '^').gravity('Center').extent('300', '300').stream().pipe(writeStream);
 };
 
-var imageStore = new FS.Store.GridFS("images");
+var crop = function(fileObj, readStream, writeStream) {
+  //  transforms image into 700x700px square
+  gm(readStream,fileObj.name()).resize('700','700' + '^').gravity('Center').extent('700', '700').stream().pipe(writeStream);
+};
+
+var imageStore = new FS.Store.GridFS("images", {transformWrite: crop });
 var thumbStore = new FS.Store.GridFS("thumbs", {path:" /uploads/thumbs", transformWrite: createThumb });
 Images = new FS.Collection("images", {
   stores: [
