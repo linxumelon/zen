c20a01bd8e77869932027fcb52fea366caaf9c1e
Xiao Yilin <>
2018-06-27 18:43:47 +0800

    alerts url

--- client/lib/choosePage.js
+++ client/lib/choosePage.js
@@ -16,6 +16,7 @@ Template.choosePage.helpers({
 Template.choosePage.events({
 	'click .pure-u-1-4': function() {
 		var imageURL = this.url("cropped");
+		alert(imageURL);
 		var params = {};
 		var queryParams = {image: imageURL}
 		FlowRouter.go("/color", params, queryParams);
