87de74cc21de13f57292e3c570e032fff915ee1a
Xiao Yilin <>
2018-07-19 16:53:49 +0800

    can remove images

--- client/lib/choosePage.js
+++ client/lib/choosePage.js
@@ -16,9 +16,9 @@ Template.choosePage.helpers({
 Template.choosePage.events({
 	'click .pure-u-1-4': function() {
 		var imageURL = this.url("cropped");
-		// for deleting images: Images.remove(this._id);
+		Images.remove(this._id);
 		var params = {};
 		var queryParams = {image: imageURL}
-		FlowRouter.go("/color", params, queryParams);
+		//FlowRouter.go("/color", params, queryParams);
 	}
 })
